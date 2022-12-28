e5eb9364e0a842cbef784a258c0c98af8e6e12f8
omarsal3m <0marsal3m@gmail.com>
2022-12-27 22:21:58 -0700

    Add code to implement scatter graphs using plt.plot(...) & plt.scatter()

--- main.py
+++ main.py
@@ -16,3 +16,17 @@ plt.plot(x,y)
 plt.show()
 # You always need to write plt-show() in order to see the graph
 # plt-show()  # this shows the graph
+
+# Plotting several points in matplotlib
+# Each numpy array has to be the same length
+x = np.array([0, 30, 12, 56, 19, 100])
+y = np.array([40, 72, 80, 13, 17, 39])
+
+# Instead of a line graph we want a scatter graph
+# Method #1
+plt.plot(x, y, 's')
+plt.show()
+
+# Method #2
+plt.scatter(x, y)
+plt.show()
