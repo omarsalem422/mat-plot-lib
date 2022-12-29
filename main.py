89592f5611ce7c77eae013c42ecaeb7459bfce87
omarsalem422 <0marsal3m@gmail.com>
2022-12-29 14:14:29 -0700

    Plotting more than one graph on a plot

--- main.py
+++ main.py
@@ -56,3 +56,17 @@ y = np.array([20, 30, 50, 100, 101])
 plt.plot(x, y, c='black', marker='*', linestyle='--', markersize=10, markerfacecolor='blue',
          markeredgecolor='blue', linewidth=2)
 plt.show()
+
+# PLotting more than one graph on a plot - actually 3 plots!
+x = np.array([0, 10, 100])
+y = np.array([0, 1, 2])
+plt.plot(x,y)
+
+r = np.array([0, 50, 10])
+s = np.array([0, 46, 92])
+plt.plot(r,s)
+
+a = np.array([0, 80, 100])
+b = np.array([0, 40,92])
+plt.plot(a,b)
+plt.show()
